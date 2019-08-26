179deeb5ed0977e4f67e32135c4ecc2d95d7a065
Justd <IsZhangjd@outlook.com>
2019-08-26 23:56:37 +0800

    eureka

--- microservicecloud-eureka-7001/src/main/java/org/justd/springcloud/EurekaServer7001.java
+++ microservicecloud-eureka-7001/src/main/java/org/justd/springcloud/EurekaServer7001.java
@@ -2,17 +2,18 @@ package org.justd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author: zhangjd
- * @Date: 2019/8/21 01:32
+ * @Date: 2019/8/23 00:16
  * @Description:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider8001_App {
+@EnableEurekaServer
+public class EurekaServer7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(EurekaServer7001.class,args);
     }
 }
